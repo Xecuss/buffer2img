ea1c79acec0059a955b7b1ba9f9cf91485d7f9a5
likai <likai@mgtv.com>
2021-10-15 20:19:43 +0800

    修改打包位置

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'docs'
+  },
   plugins: [svelte()],
   server: {
     port: 5000,
