4fd981ae512c46475de6c77c7353bdbcf6f7a019
likai <likai@mgtv.com>
2021-10-21 15:21:19 +0800

    uInt8array切割方法

--- src/lib/procFile.ts
+++ src/lib/procFile.ts
@@ -10,7 +10,14 @@ export default async function procFile(file: File) {
     const zipedData = zlibSync(rawData, { level: 9 });
 }
 
-function getLength16(length: number) {
+function createHeader(files: File[]) {
+    // header由几个整数组成
+    // 1. 版本号 - 目前固定为1
+    // 2. 图片数量 n
+    // 3 - n + 3 每张图片的大小
+}
+
+function int2bits(length: number) {
     let lengthStr = length.toString(2);
     const res = [];
     while(lengthStr.length > 0) {
@@ -26,7 +33,7 @@ function getLength16(length: number) {
     return res;
 }
 
-function calcLength16(numArr: number[]) {
+function bits2int(numArr: number[]) {
     let resStr = '';
     for(let item of numArr) {
         const bits = ('00000000' + item.toString(2)).slice(-8);
